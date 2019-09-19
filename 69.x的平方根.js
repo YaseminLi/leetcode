e35aa8305aea732405ba8b76e47758aa11b56ae4
YaseminLi <yaseminli@163.com>
2019-09-19 16:47:08 +0800

    x的平方根

--- 69.x的平方根.js
+++ 69.x的平方根.js
@@ -3,24 +3,42 @@
  * @return {number}
  */
 var mySqrt = function (x) {
-    if(x==1||x==0){
+    //1⃣️排除特殊情况
+    if (x == 1 || x == 0) {
         return x
     }
-    for (let i = Math.floor(x / 2); i >=1;) {
-        if (i * i > x) {
-            i = Math.floor(i / 2)
+    if (x > 1 && x < 4) {
+        return 1
+    }
+    //平方根不会大于数字的一半
+    let l = 1;
+    let r = Math.floor(x / 2)+1
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2)
+        if (mid * mid === x) {
+            return mid
         }
-        if (i * i < x) {
-            if ((i + 1) * (i + 1) > x) {
-                return i
-            } else { i += 1 }
-        }
-        if (i * i == x) {
-            return i
+        if (mid * mid < x) {
+            if ((mid + 1) * (mid + 1) > x) {
+                return mid
+            } if ((mid + 1) * (mid + 1) == x) {
+                return mid + 1
+            }
+            l = mid
+        } else {
+            if ((mid - 1) * (mid - 1) <x) {
+                return mid-1
+            }
+            if ((mid - 1) * (mid -1) == x) {
+                return mid 
+            }
+            r = mid - 1
         }
     }
+
 };
-console.log(mySqrt(8));
-console.log(mySqrt(6));
-console.log(mySqrt(4));
-console.log(mySqrt(1));
+// console.log(mySqrt(8));
+// console.log(mySqrt(6));
+console.log(mySqrt(36));
+// console.log(mySqrt(4));
+// console.log(mySqrt(1));
