910ed4a34cc14b0a6b5da86f1f73d99d2cecc484
YaseminLi <yaseminli@163.com>
2020-02-02 21:04:07 +0800

    双指针

--- 题解/双指针/167.两数之和 II - 输入有序数组.js
+++ 题解/双指针/167.两数之和 II - 输入有序数组.js
@@ -17,7 +17,7 @@ easy
  */
 
 
-//双层循环
+//双层循环，太慢了……
 //1.外层循环固定第一个值first，从头部开始
 //2.内层循环查找第二个值second,从尾部开始。因为是有序数组，当比较的值小于second时，结束这层循环
 //3.返回索引（注意从1开始）
@@ -26,22 +26,55 @@ easy
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(numbers, target) {
-    const len=numbers.length
-    let result=null
-    for(let i=0;i<len;i++){
-        const first=numbers[i]
-        const second=target-first
-        for(let j=len-1;j>i;j--){
-            if(numbers[j]===second){
-                return result=[i+1,j+1]
-            }else if(numbers[j]<second){
-                break
-            }
+// var twoSum = function(numbers, target) {
+//     const len=numbers.length
+//     let result=null
+//     for(let i=0;i<len;i++){
+//         const first=numbers[i]
+//         const second=target-first
+//         for(let j=len-1;j>i;j--){
+//             if(numbers[j]===second){
+//                 return result=[i+1,j+1]
+//             }else if(numbers[j]<second){
+//                 break
+//             }
+//         }
+//     }
+//     return result
+// };
+// const numbers=[2,7,9,18,20,20,36,48,50]
+// const target=22
+// console.log(twoSum(numbers,target))
+
+//1.第一个指针从头部开始，第二个指针从尾部开始
+//2.两个指针指向的sum与target比较，sum<target 移动第一个指针，相反就移动第二个指针
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+    const len = numbers.length
+    if (len < 2) {
+        return null
+    }
+    let result = null
+    let l = 0
+    let r = len - 1
+    while (l < r) {
+        let sum = numbers[l] + numbers[r]
+        if (sum < target) {
+            l++
+        } else if (sum > target) {
+            r--
+        } else {
+            result = [l + 1, r + 1]
+            break
         }
     }
     return result
 };
-const numbers=[2,7,9,18,20,20,36,48,50]
-const target=22
-console.log(twoSum(numbers,target))
+const numbers = [2, 7, 9, 18, 20, 20, 36, 48, 50]
+const target = 22
+console.log(twoSum(numbers, target))
